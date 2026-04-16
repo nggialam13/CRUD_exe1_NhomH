7816c94b2c74e48715efcffc7559c82a3d98525c
pdHanh <pdhanh.12a3.2020@gmail.com>
2026-04-17 03:52:50 +0700

    fix bug register

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,12 +14,13 @@ Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/logout', [UserController::class, 'logout'])->name('logout');
 // 
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+Route::post('/users/store', [UserController::class, 'store'])->name('users.store');
 
 // Nhóm route chỉ dành cho admin (role = 1)
 Route::middleware(['auth', 'role:1'])->group(function () {
     
-    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-    Route::post('/users/store', [UserController::class, 'store'])->name('users.store');
+    
     Route::get('/users/edit/{id}', [UserController::class, 'edit'])->name('users.edit');
     Route::post('/users/update/{id}', [UserController::class, 'update'])->name('users.update');
     Route::delete('/users/delete/{id}', [UserController::class, 'destroy'])->name('users.delete');
